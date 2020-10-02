5496be5d1f0446aae01453855a554b6622f804f6
Sandy <sandychn@163.com>
2020-10-03 00:47:15 +0800

    update 0001

--- Easy/0001-two-sum.cpp
+++ Easy/0001-two-sum.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        const size_t n = nums.size();
+
         unordered_map<int, int> hash;
-        for (size_t i = 0; i < nums.size(); ++i) {
+        for (size_t i = 0; i < n; ++i) {
             hash.emplace(nums[i], i);
             auto it = hash.find(target - nums[i]);
             if (it != hash.end() && it->second != i) {
-                return vector<int>{i, it->second};
+                return vector<int>{static_cast<int>(i), static_cast<int>(it->second)};
             }
         }
         return vector<int>();
