13610dacf17d16f231e987299de50dbf4a4f566e
Sandy <sandychn@163.com>
2020-10-04 00:47:52 +0800

    0002

---
+++ Easy/0002-add-two-numbers.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode head;
+        ListNode *p = &head;
+        int c = 0;
+        while (l1 != nullptr || l2 != nullptr || c > 0) {
+            if (l1 != nullptr) {
+                c += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                c += l2->val;
+                l2 = l2->next;
+            }
+            p->next = new ListNode(c % 10);
+            p = p->next;
+            c /= 10;
+        }
+        return head.next;
+    }
+};
