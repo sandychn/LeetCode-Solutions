af044f8ee87740cde81ad45e1cf8c9264945cdf9
Sandy <sandychn@163.com>
2020-11-10 00:24:39 +0800

    0031

---
+++ Medium/0031-next-permutation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.empty()) return;
+        int size = nums.size();
+        int i = size - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+        if (i >= 0) {
+            int j = size - 1;
+            while (j >= 0 && nums[i] >= nums[j]) --j;
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
