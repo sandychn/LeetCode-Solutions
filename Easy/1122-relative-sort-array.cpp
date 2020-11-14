acd00b72d2a3084c6975dde474dc843666b16f67
Sandy <sandychn@163.com>
2020-11-14 15:48:20 +0800

    update 1122: another approach using counting

--- Easy/1122-relative-sort-array.cpp
+++ Easy/1122-relative-sort-array.cpp
@@ -4,6 +4,10 @@ public:
     static constexpr int INF = 0x3F3F3F3F;
 
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        return solver2(arr1, arr2);
+    }
+
+    vector<int> solver1(vector<int>& arr1, vector<int>& arr2) {
         vector<int> id(MAX_VALUE + 5, INF);
         for (int i = 0, n = arr2.size(); i < n; i++) {
             id[arr2[i]] = i;
@@ -16,5 +20,26 @@ public:
         };
         sort(arr1.begin(), arr1.end(), compare);
         return arr1;
+    }
+
+    vector<int> solver2(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> cnt(MAX_VALUE + 5);
+        for (int value : arr1) ++cnt[value];
+
+        vector<int> res;
+        res.reserve(arr1.size());
+        for (int value : arr2) {
+            while (cnt[value] > 0) {
+                --cnt[value];
+                res.push_back(value);
+            }
+        }
+        for (int i = 0; i <= MAX_VALUE; i++) {
+            while (cnt[i] > 0) {
+                --cnt[i];
+                res.push_back(i);
+            }
+        }
+        return res;
     }
 };
