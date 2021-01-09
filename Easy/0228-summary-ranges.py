8480955fbb4874ddf9620e91146f15bdf42ccdbd
Sandy <sandychn@163.com>
2021-01-10 00:42:33 +0800

    0228

---
+++ Easy/0228-summary-ranges.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        def updateAnswer(res, x, y):
+            if x is None:
+                return
+            if x == y - 1:
+                ans.append(str(x))
+            else:
+                ans.append("{}->{}".format(x, y - 1))
+
+        ans = []
+        start = expected = None
+        i, n = 0, len(nums)
+        for val in nums:
+            if val == expected:
+                expected += 1
+            else:
+                updateAnswer(ans, start, expected)
+                start, expected = val, val + 1
+        updateAnswer(ans, start, expected)
+        return ans
