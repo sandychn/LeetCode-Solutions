f9762403076b6cf9d4005f8f4334f70d27442553
Sandy <sandychn@163.com>
2021-02-13 00:18:24 +0800

    Update 0448: restore values in the array

--- Easy/0448-find-all-numbers-disappeared-in-an-array.py
+++ Easy/0448-find-all-numbers-disappeared-in-an-array.py
@@ -3,4 +3,8 @@ class Solution:
         for v in nums:
             v = abs(v)
             nums[v - 1] = -abs(nums[v - 1])
-        return [(i + 1) for i in range(len(nums)) if nums[i] > 0]
+        res = [(i + 1) for i in range(len(nums)) if nums[i] > 0]
+        # restore values in the array
+        for i in range(len(nums)):
+            nums[i] = abs(nums[i])
+        return res
