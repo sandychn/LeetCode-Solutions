a63e74e3b67e59217c364c4d47bdf3b5ac3ee149
Sandy <sandychn@163.com>
2021-03-03 00:35:13 +0800

    0338

---
+++ Medium/0338-counting-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res = [0] * (num + 1)
+        for i in range(1, num + 1):
+            res[i] = 1 + res[i & (i - 1)]
+            # or: res[i] = res[i >> 1] + (i & 1)
+        return res
