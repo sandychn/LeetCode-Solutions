283a98b9828d896fc45a4572cf25ed1046bcdea5
Sandy <sandychn@163.com>
2021-03-06 02:23:58 +0800

    Update DepthFirstSearch 0131

--- Algorithms/DepthFirstSearch/0131-palindrome-partitioning.py
+++ Algorithms/DepthFirstSearch/0131-palindrome-partitioning.py
@@ -3,13 +3,13 @@ HASH_VALUE_MAX = (1 << 32) - 1
 
 class Solution:
     def solve(self, s: str, start: int, pos: int, fwdHash: int, bwdHash: int, times: int, memory: dict):
+        if start >= len(s):
+            return []
         m = memory.get((start, pos))
         if m is not None:
             return m
-        if start >= len(s):
-            return []
         if pos == len(s):
-            res = memory[(start, pos)] = [[(start, pos)]] if fwdHash == bwdHash else []
+            res = memory[(start, pos)] = [[s[start:pos]]] if fwdHash == bwdHash else []
             return res
         ch = s[pos]
         fwdHash = (fwdHash * SEED + ord(ch)) & HASH_VALUE_MAX
@@ -17,10 +17,9 @@ class Solution:
         times = (times * SEED) & HASH_VALUE_MAX
         res = self.solve(s, start, pos + 1, fwdHash, bwdHash, times, memory)
         if fwdHash == bwdHash:
-            res.extend([(start, pos + 1)] + x for x in self.solve(s, pos + 1, pos + 1, 0, 0, 1, memory))
+            res.extend([s[start:pos + 1]] + x for x in self.solve(s, pos + 1, pos + 1, 0, 0, 1, memory))
         memory[(start, pos)] = res
         return res
 
     def partition(self, s: str) -> List[List[str]]:
-        res = self.solve(s, 0, 0, 0, 0, 1, {})
-        return [[s[left:right] for left, right in x] for x in res]
+        return self.solve(s, 0, 0, 0, 0, 1, {})
