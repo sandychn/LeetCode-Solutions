c5979cf7ae170590e99313caffd9e029c248e897
Sandy <sandychn@163.com>
2021-03-07 00:42:54 +0800

    Update DepthFirstSearch 0131

--- Algorithms/DepthFirstSearch/0131-palindrome-partitioning.py
+++ Algorithms/DepthFirstSearch/0131-palindrome-partitioning.py
@@ -2,24 +2,25 @@ SEED = 131
 HASH_VALUE_MAX = (1 << 32) - 1
 
 class Solution:
-    def solve(self, s: str, start: int, pos: int, fwdHash: int, bwdHash: int, times: int, memory: dict):
-        if start >= len(s):
+    def solve(self, start: int, pos: int, fwdHash: int, bwdHash: int, times: int, memory: Dict[Tuple[int, int], List[List[str]]]):
+        if start >= len(self.s):
             return []
         m = memory.get((start, pos))
         if m is not None:
             return m
-        if pos == len(s):
-            res = memory[(start, pos)] = [[s[start:pos]]] if fwdHash == bwdHash else []
+        if pos == len(self.s):
+            res = memory[(start, pos)] = [[self.s[start:pos]]] if fwdHash == bwdHash else []
             return res
-        ch = s[pos]
+        ch = self.s[pos]
         fwdHash = (fwdHash * SEED + ord(ch)) & HASH_VALUE_MAX
         bwdHash = (bwdHash + ord(ch) * times) & HASH_VALUE_MAX
         times = (times * SEED) & HASH_VALUE_MAX
-        res = self.solve(s, start, pos + 1, fwdHash, bwdHash, times, memory)
+        res = self.solve(start, pos + 1, fwdHash, bwdHash, times, memory)
         if fwdHash == bwdHash:
-            res.extend([s[start:pos + 1]] + x for x in self.solve(s, pos + 1, pos + 1, 0, 0, 1, memory))
+            res.extend([self.s[start:pos + 1]] + x for x in self.solve(pos + 1, pos + 1, 0, 0, 1, memory))
         memory[(start, pos)] = res
         return res
 
     def partition(self, s: str) -> List[List[str]]:
-        return self.solve(s, 0, 0, 0, 0, 1, {})
+        self.s = s
+        return self.solve(0, 0, 0, 0, 1, {})
