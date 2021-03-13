d6d73bfbbe2ed4d6b80cf5c983278e793d615ebe
Sandy <sandychn@163.com>
2021-03-14 00:32:45 +0800

    Update Trie 0208

--- Algorithms/Trie/0208-implement-trie-prefix-tree.py
+++ Algorithms/Trie/0208-implement-trie-prefix-tree.py
@@ -6,17 +6,12 @@ class Trie:
 
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.tree = [Trie.TrieNode()]
+        self.root = Trie.TrieNode()
 
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        node = self.tree[0]
+        """Inserts a word into the trie."""
+        node = self.root
         for ch in map(lambda ch: ord(ch) - ord("a"), word):
             if node.next[ch] is None:
                 node.next[ch] = Trie.TrieNode()
@@ -25,32 +20,24 @@ class Trie:
 
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
+        """Returns if the word is in the trie."""
         node = self.getNode(word)
         return node is not None and node.cnt > 0
 
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
+        """Returns if there is any word in the trie that starts with the given prefix."""
         node = self.getNode(prefix)
         return node is not None
 
 
     def getNode(self, word: str) -> TrieNode:
-        node = self.tree[0]
+        """Returns the TrieNode where the given word ends,
+        or None if the given word not exists in the Trie.
+        """
+        node = self.root
         for ch in map(lambda ch: ord(ch) - ord("a"), word):
             node = node.next[ch]
             if not node:
                 break
         return node
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
