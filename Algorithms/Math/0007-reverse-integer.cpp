fdcea4b1da189dc29ef2cb5e1435c08ef82c9bbc
Sandy <sandychn@163.com>
2021-05-03 00:34:42 +0800

    Math 0007 Optimized

--- Algorithms/Math/0007-reverse-integer.cpp
+++ Algorithms/Math/0007-reverse-integer.cpp
@@ -12,9 +12,19 @@ public:
         while (x) {
             int digit = x % 10;
             if (ans < 0) {
-                if (ans < INT_MIN_DIV_10 || (ans == INT_MIN_DIV_10 && digit < INT_MIN_MOD_10)) return OVERFLOW;
+                // if (ans < INT_MIN_DIV_10 || (ans == INT_MIN_DIV_10 && digit < INT_MIN_MOD_10)) return OVERFLOW;
+                // 当 ans == INT_MIN_DIV_10 时, 说明输入值 x 也为 10 位,
+                // 则其最高位为 1 或 2, 即 digit 为 -1 或 -2,
+                // 由于 INT_MIN_MID_10 = -8, 所以 digit < INT_MIN_MID_10 不会成立
+                // 即 (ans == INT_MIN_DIV_10 && digit < INT_MIN_MOD_10) 一定为 false
+                if (ans < INT_MIN_DIV_10) return OVERFLOW;
             } else {
-                if (ans > INT_MAX_DIV_10 || (ans == INT_MAX_DIV_10 && digit > INT_MAX_MOD_10)) return OVERFLOW;
+                // if (ans > INT_MAX_DIV_10 || (ans == INT_MAX_DIV_10 && digit > INT_MAX_MOD_10)) return OVERFLOW;
+                // 当 ans == INT_MAX_DIV_10 时, 说明输入值 x 也为 10 位,
+                // 则其最高位为 1 或 2, 即 digit 为 1 或 2,
+                // 由于 INT_MAX_MOD_10 = 7, 所以 digit < INT_MAX_MOD_10 不会成立
+                // 即 (ans == INT_MAX_DIV_10 && digit > INT_MAX_MOD_10) 一定为 false
+                if (ans > INT_MAX_DIV_10) return OVERFLOW;
             }
             ans = ans * 10 + digit;
             x /= 10;
